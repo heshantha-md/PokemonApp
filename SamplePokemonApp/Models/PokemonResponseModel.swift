e9a496bf53ce97c2ebadf9c287ad584ba25b488b
Heshantha Mahawaththage Don <reach.heshantha@icloud.com>
2024-06-06 12:13:36 +0100

    In-Progress: API Fetch Error, "The data couldn’t be read because it is missing" for "getPokemon" endpoint.

---
+++ SamplePokemonApp/Models/PokemonResponseModel.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonResponseModel.swift
+//  SamplePokemonApp
+//
+//  Created by Heshantha Don on 06/06/2024.
+//
+
+struct PokemonResponseModel: Decodable {
+    // MARK: - PROPERTIES
+    let name: String
+    let url: String
+}
---
+++ SamplePokemonApp/Utils/ApiError.swift
@@ -0,0 +1,12 @@
+//
+//  ApiError.swift
+//  SamplePokemonApp
+//
+//  Created by Heshantha Don on 06/06/2024.
+//
+
+import Foundation
+
+enum ApiError: LocalizedError {
+    case badUrl, badRequest
+}
