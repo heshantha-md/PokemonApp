e8d2a33a7e38cd849db7930d0fff377aac8087df
Heshantha Mahawaththage Don <reach.heshantha@icloud.com>
2024-06-06 08:42:06 +0100

    Initial Commit

---
+++ SamplePokemonApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SamplePokemonApp
+//
+//  Created by Heshantha Don on 06/06/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
